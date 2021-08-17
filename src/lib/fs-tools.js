e11e93ef3d0284a1caa62b594cd92db423a3a6e2
Sarath-97 <sarath559709@gmail.com>
2021-08-17 15:47:55 +0100

    updates

--- src/lib/fs-tools.js
+++ src/lib/fs-tools.js
@@ -4,12 +4,12 @@ import { dirname, join } from "path"
 
 const { readJSON, writeJSON, writeFile } = fs
 
-const studentsJSONPath = join(dirname(fileURLToPath(import.meta.url)), "../../author.json")
+const authorJSONPath = join(dirname(fileURLToPath(import.meta.url)), "../data/author.json")
 const booksJSONPath = join(dirname(fileURLToPath(import.meta.url)), "../data/books.json")
 const publicFolderPath = join(dirname(fileURLToPath(import.meta.url)), "../../public/img/students")
 
-export const getAuthor = () => readJSON(studentsJSONPath)
-export const writeAuthor = content => writeJSON(studentsJSONPath, content)
+export const getAuthor = () => readJSON(authorJSONPath)
+export const writeAuthor = content => writeJSON(authorJSONPath, content)
 export const getBooks = () => readJSON(booksJSONPath)
 export const writeBooks = content => writeJSON(booksJSONPath, content)
 
--- src/services/books/index.js
+++ src/services/books/index.js
@@ -1,69 +1,95 @@
-import express from 'express'
-import fs from 'fs'
-import { getBooks, writeBooks } from '../../lib/fs-tools'
-import uniqid from "uniqid"
+import express from "express";
+import { getBooks, writeBooks } from "../../lib/fs-tools";
+import uniqid from "uniqid";
+
+const booksRouter = express.Router();
+
+booksRouter.get("/", async (req, res, next) => {
+  try {
+    const books = await getBooks();
+    console.log("Qurey params = ", req.query);
+    if (req.query && req.query.title) {
+      const filteredBooks = books.filter((b = b.title === req.params.title));
+      res.send(filteredBooks);
+    } else {
+      res.send(books);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+booksRouter.get("/:id", async (req, res, next) => {
+  try {
+
+    const books = await getBooks();
+
+    const book = books.find((b) => b.id === req.params.id);
+    
+    
+    res.send(book);
+  
+  } catch (error) {
+    next(error)
+  }
+});
+
+booksRouter.post("/", async (req, res, next) => {
+
+  try {
+    
+    const books = await getBooks();
+  const newBook = {id: uniqid(), ...req.body, createdAt: new Date() };
+
+  books.push(newBook);
+
+  writeBooks(books);
+
+  res.status(201).send(newBook);
+
+  } catch (error) {
+    next(error)
+  }
+
+});
+
+booksRouter.put("/:id", async (req, res, next) => {
+  const books = await getBooks();
+
+  try {
+
+    const remainingBooks = books.filter((b) => b.id !== req.params.id);
+
+  const modifiedBook = { ...req.body, id: req.params.id };
+
+  remainingBooks.push(modifiedBook);
+
+  await writeBooks(remainingBooks);
+
+  res.send(modifiedBook);
+    
+  } catch (error) {
+    next(error)
+  }
 
 
-const booksRouter = express.Router()
+});
 
-booksRouter.get("/", async (req, res,next) => {
+booksRouter.delete("/:id", async (req, res, next) => {
 
- try {
+  try {
+    const books = await getBooks();
 
-  const books = await getBooks()
+  const remainingBooks = books.filter((b) => b.id !== req.params.id);
 
-  res.send(books)
-   
- } catch (error) {
-   next(error)
- }
+  writeBooks(remainingBooks);
 
-})
+  res.status(204).send();
+    
+  } catch (error) {
+    next(error)
+  }
+  
+});
 
-booksRouter.get("/:id", async (req, res,next) => {
-  const books = await getBooks()
-
-  const book = books.find(b => b.id === req.params.id)
-  res.send(book)
-})
-
-booksRouter.post("/", async (req, res,next) => {
-
-  const books = await getBooks()
-  const newBook = {...req.body, id: uniqid(), createdAt: new Date()}
-
-  books.push(newBook)
-
-  writeBooks(books)
-
-  res.status(201).send(newBook)
-})
-
-booksRouter.put("/:id", async (req, res,next) => {
-  const books = await getBooks()
-
-  const remainingBooks = books.filter(b => b.id !== req.params.id)
-
-  const modifiedBook = {...req.body, id: req.params.id}
-
-  remainingBooks.push(modifiedBook) 
-
-  writeBooks(remainingBooks)
-
-  res.send(modifiedBook)
-
-})
-
-booksRouter.delete("/:id", async (req, res,next) => {
-
-  const books = await getBooks()
-
-  const remainingBooks = books.filter(b => b.id !== req.params.id)
-
-  writeBooks(remainingBooks)
-
-  res.status(204).send()
-})
-
-export default booksRouter
-
+export default booksRouter;
