798429f7dac9c020d40db3db0c7d355dc99de388
Sarath-97 <sarath559709@gmail.com>
2021-08-18 15:22:01 +0100

    test with transforming json to zip

--- stream-examples.js
+++ stream-examples.js
@@ -2,10 +2,22 @@ import request from "request"
 import { pipeline } from "stream"
 import fs from "fs"
 
-const source = fs.createReadStream("./src/data/books.json")
-const desitnation = process.stdout
+/* ********* STREAMING DATA TO NEW JSON FILE************* */
+// const source = fs.createReadStream("./src/data/books.json")
+// const desitnation = fs.createWriteStream("stream.json")
 
-pipeline(source,desitnation, err => {
-    if(err) console.log(err);
-    console.log("Stream successsful");
+// pipeline(source,desitnation, err => {
+//     if(err) console.log(err);
+//     console.log("Stream successsful");
+// })
+
+import { createGzip } from "zlib"
+/* ********* tRANSFORMING FILE TO ANOTHER FILE EXTENSION TO NEW JSON FILE************* */
+
+const source =fs.createReadStream("./src/data/author.json")
+const desitnation= fs.createWriteStream("author.json.gz")
+const transform = createGzip()
+pipeline(source, transform,desitnation, err =>{
+    if(err)console.log(err);
+    console.log("Successfully transformed and ended");
 })
